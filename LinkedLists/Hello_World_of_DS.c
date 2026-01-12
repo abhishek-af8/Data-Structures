be18639bb74f45b57e01fa8b21c9bc7f8cbd8e3c
Abhishek <abhishektiwari.af8@gmail.com>
2026-01-12 21:13:11 +0530

    wrote the Hello World of DSA

---
+++ LinkedLists/Hello_World_of_DS.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    // printf("Welcome to learn Linked Lists");
+    // printf("\nYou will become an excellent programmer\n");
+
+    struct node a, b, c;
+
+    a.data = 21;
+    a.next = &b;
+
+    b.data = 22;
+    b.next = &c;
+
+    c.data = 23;
+    c.next = NULL;
+
+    struct node *head;
+    head = &a;
+
+    printf("%d\n", head->next->next->data);
+    
+return 0;
+}
--- LinkedLists/LL1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("Welcome to learn Linked Lists");
-    printf("\nYou will become an excellent programmer\n");
-    
-return 0;
-}
